7eeac20bb40cd9568dea9aed2b1c858b5d339e86
James <jbean46@gmail.com>
2018-05-23 13:37:06 -0400

    Rename *Ordinal -> *Size

--- Sources/SpelledPitch/NamedIntervalOrdinal.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NamedIntervalOrdinal.swift
-//  SpelledPitch
-//
-//  Created by James Bean on 5/20/18.
-//
-
-/// Interface for `NamedIntervalOrdinal`-like values.
-public protocol NamedIntervalOrdinal: Invertible { }
---
+++ Sources/SpelledPitch/NamedIntervalSize.swift
@@ -0,0 +1,9 @@
+//
+//  NamedIntervalSize.swift
+//  SpelledPitch
+//
+//  Created by James Bean on 5/20/18.
+//
+
+/// Interface for `NamedIntervalSize`-like values.
+public protocol NamedIntervalSize: Invertible { }
