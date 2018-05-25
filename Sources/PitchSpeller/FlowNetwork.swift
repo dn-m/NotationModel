7676371670b5c4e00d08bed70caa097236a5ef31
James <jbean46@gmail.com>
2018-05-24 20:16:14 -0400

    Move FlowNetwork to own file

---
+++ Sources/PitchSpeller/FlowNetwork.swift
@@ -0,0 +1,96 @@
+//
+//  FlowNetwork.swift
+//  PitchSpeller
+//
+//  Created by James Bean on 5/24/18.
+//
+
+import Pitch
+
+extension Wetherfield {
+
+    public struct FlowNetwork {
+
+        struct UnassignedNodeInfo: Hashable {
+
+            /// The `pitchClass` which is being represented by a given `Node`.
+            let pitchClass: Pitch.Class
+
+            /// Index of the node in the `Box` for the given `pitchClass`. Will be either `0`, or
+            /// `1`. This value will ultimately represent the index within a `TendencyPair`.
+            let index: Int
+        }
+
+        struct AssignedNodeInfo: Hashable {
+
+            /// The `pitchClass` which is being represented by a given `Node`.
+            let pitchClass: Pitch.Class
+
+            /// Index of the node in the `Box` for the given `pitchClass`. Will be either `0`, or
+            /// `1`. This value will ultimately represent the index within a `TendencyPair`.
+            let index: Int
+
+            /// The "tendency" value assigned subsequent to finding the minium cut.
+            let tendency: PitchSpeller.Category.Tendency
+
+            // MARK: - Initializers
+
+            init(_ nodeInfo: UnassignedNodeInfo, tendency: PitchSpeller.Category.Tendency) {
+                self.pitchClass = nodeInfo.pitchClass
+                self.index = nodeInfo.index
+                self.tendency = tendency
+            }
+        }
+
+        // TODO: Consider more (space-)efficient storage of Nodes.
+        internal var graph: Graph<UnassignedNodeInfo>
+        internal var source: Graph<UnassignedNodeInfo>.Node
+        internal var sink: Graph<UnassignedNodeInfo>.Node
+        internal var internalNodes: [Graph<UnassignedNodeInfo>.Node] = []
+
+        public init(pitchClasses: Set<Pitch.Class>, parsimonyPivot: Pitch.Class = 2) {
+
+            // Create an empty `Graph`.
+            self.graph = Graph()
+
+            // Create the `source` node of the pair representing the `parsimony pivot`.
+            self.source = self.graph.createNode(
+                UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 0)
+            )
+
+            // Create the `sink` nodeof the pair representing the `parsimony pivot`.
+            self.sink = self.graph.createNode(
+                UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 1)
+            )
+
+            // Create nodes for each pitch class in the given `pitchClasses`.
+            for pitchClass in pitchClasses {
+
+                // Create two nodes, one which will represent the `up` tendency (index 0), and the
+                // other which will represent the `down` tendency (index 1)
+                for index in [0,1] {
+                    internalNodes.append(
+                        self.graph.createNode(
+                            UnassignedNodeInfo(pitchClass: pitchClass, index: index)
+                        )
+                    )
+                }
+            }
+
+            // Connect nodes
+            for node in internalNodes {
+
+                // Add edges from source to all internal nodes, with an initial value of 1.
+                self.graph.addEdge(from: source, to: node, value: 1)
+
+                // Add edges from all internal nodes to sink, with an initial value of 1.
+                self.graph.addEdge(from: node, to: sink, value: 1)
+
+                // Add edges from all internal nodes to all other internal nodes.
+                for other in internalNodes.lazy.filter({ $0 != node }) {
+                    self.graph.addEdge(from: node, to: other, value: 1)
+                }
+            }
+        }
+    }
+}
