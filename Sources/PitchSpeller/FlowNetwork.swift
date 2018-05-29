591bb23a17dfb529daae83c20743a4bbc96c1ddc
James <jbean46@gmail.com>
2018-05-29 10:07:56 -0700

    Make function to create flow network for pitch classes

--- Sources/PitchSpeller/FlowNetwork.swift
+++ Sources/PitchSpeller/FlowNetwork.swift
@@ -48,6 +48,60 @@ extension Queue: ExpressibleByArrayLiteral {
 
 extension Wetherfield {
 
+    // TODO: Expand this out so that each "notehead" (pitch-event) has a `box` of two nodes as
+    // opposed to only a single `Pitch.Class` having a `box`.
+    func flowNetwork(
+        pitchClasses: Set<Pitch.Class>,
+        parsimonyPivot: Pitch.Class = 2
+    ) -> FlowNetwork<UnassignedNodeInfo>
+    {
+
+        // Create an empty `Graph`.
+        var graph = Graph<UnassignedNodeInfo>()
+
+        // Create the `source` node of the pair representing the `parsimony pivot`.
+        let source = graph.createNode(
+            UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 0)
+        )
+
+        // Create the `sink` node of the pair representing the `parsimony pivot`.
+        let sink = graph.createNode(
+            UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 1)
+        )
+
+        var internalNodes: [Graph<UnassignedNodeInfo>.Node] = []
+
+        // Create nodes for each pitch class in the given `pitchClasses`.
+        for pitchClass in pitchClasses {
+
+            // Create two nodes, one which will represent the `up` tendency (index 0), and the
+            // other which will represent the `down` tendency (index 1)
+            for index in [0,1] {
+                internalNodes.append(
+                    graph.createNode(UnassignedNodeInfo(pitchClass: pitchClass, index: index))
+                )
+            }
+        }
+
+        // Connect nodes
+        for node in internalNodes {
+
+            // Add edges from source to all internal nodes, with an initial value of 1.
+            graph.addEdge(from: source, to: node, value: 1)
+
+            // Add edges from all internal nodes to sink, with an initial value of 1.
+            graph.addEdge(from: node, to: sink, value: 1)
+
+            // Add edges from all internal nodes to all other internal nodes.
+            // TODO: Ensure `filter` does not making this accidentally expensive.
+            for other in internalNodes.lazy.filter({ $0 != node }) {
+                graph.addEdge(from: node, to: other, value: 1)
+            }
+        }
+
+        return FlowNetwork(graph, source: source, sink: sink)
+    }
+
     internal struct UnassignedNodeInfo: Hashable {
 
         /// The `pitchClass` which is being represented by a given `Node`.
@@ -98,54 +152,6 @@ extension Wetherfield {
             self.source = source
             self.sink = sink
         }
-
-//        // TODO: Expand this out so that each "notehead" (pitch-event) has a `box` of two nodes as
-//        // opposed to only a single `Pitch.Class` having a `box`.
-//        public init(pitchClasses: Set<Pitch.Class>, parsimonyPivot: Pitch.Class = 2) {
-//
-//            // Create an empty `Graph`.
-//            self.graph = Graph()
-//
-//            // Create the `source` node of the pair representing the `parsimony pivot`.
-//            self.source = self.graph.createNode(
-//                UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 0)
-//            )
-//
-//            // Create the `sink` node of the pair representing the `parsimony pivot`.
-//            self.sink = self.graph.createNode(
-//                UnassignedNodeInfo(pitchClass: parsimonyPivot, index: 1)
-//            )
-//
-//            // Create nodes for each pitch class in the given `pitchClasses`.
-//            for pitchClass in pitchClasses {
-//
-//                // Create two nodes, one which will represent the `up` tendency (index 0), and the
-//                // other which will represent the `down` tendency (index 1)
-//                for index in [0,1] {
-//                    internalNodes.append(
-//                        self.graph.createNode(
-//                            UnassignedNodeInfo(pitchClass: pitchClass, index: index)
-//                        )
-//                    )
-//                }
-//            }
-//
-//            // Connect nodes
-//            for node in internalNodes {
-//
-//                // Add edges from source to all internal nodes, with an initial value of 1.
-//                self.graph.addEdge(from: source, to: node, value: 1)
-//
-//                // Add edges from all internal nodes to sink, with an initial value of 1.
-//                self.graph.addEdge(from: node, to: sink, value: 1)
-//
-//                // Add edges from all internal nodes to all other internal nodes.
-//                // TODO: Ensure `filter` does not making this accidentally expensive.
-//                for other in internalNodes.lazy.filter({ $0 != node }) {
-//                    self.graph.addEdge(from: node, to: other, value: 1)
-//                }
-//            }
-//        }
 
         /// - Returns: All of the paths from the `source` to the `sink`.
         internal var paths: Set<Path> {
