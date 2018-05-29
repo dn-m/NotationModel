04b6f22be3f68ae8fb4a140c62e51c3a1e52511a
James <jbean46@gmail.com>
2018-05-29 10:08:51 -0700

    Move Queue to own file

--- Sources/PitchSpeller/FlowNetwork.swift
+++ Sources/PitchSpeller/FlowNetwork.swift
@@ -7,45 +7,6 @@
 
 import Pitch
 
-/// Queue.
-///
-/// - TODO: Move to dn-m/Structure/DataStructures
-public struct Queue <Element: Equatable> {
-
-    private var storage: [Element] = []
-
-    public var isEmpty: Bool {
-        return storage.isEmpty
-    }
-
-    public var count: Int {
-        return storage.count
-    }
-
-    public mutating func push(_ value: Element) {
-        storage.append(value)
-    }
-
-    public mutating func pop() -> Element {
-        return storage.remove(at: 0)
-    }
-
-    public func contains(_ value: Element) -> Bool {
-        if storage.index(of: value) != nil {
-            return true
-        }
-        return false
-    }
-}
-
-extension Queue: ExpressibleByArrayLiteral {
-
-    /// Create a `Queue` with an `ArrayLiteral`.
-    public init(arrayLiteral elements: Element...) {
-        self.storage = elements
-    }
-}
-
 extension Wetherfield {
 
     // TODO: Expand this out so that each "notehead" (pitch-event) has a `box` of two nodes as
