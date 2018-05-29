90dfe66be7b1bd8670e120004ad2b18dec65cc01
James <jbean46@gmail.com>
2018-05-29 10:26:05 -0700

    Move FlowNetwork out of Wetherfield namespace

--- Sources/PitchSpeller/FlowNetwork.swift
+++ Sources/PitchSpeller/FlowNetwork.swift
@@ -5,64 +5,59 @@
 //  Created by James Bean on 5/24/18.
 //
 
-import Pitch
+/// Directed Graph with several properties:
+/// - Each edge has a capacity for flow
+/// - A "source" node, which is only emanates flow outward
+/// - A "sink" node, which only receives flow
+public struct FlowNetwork <Value: Hashable>: Hashable {
 
-extension Wetherfield {
+    public typealias Path = Graph<Value>.Path
+    public typealias Node = Graph<Value>.Node
 
-    /// Directed Graph with several properties:
-    /// - Each edge has a capacity for flow
-    /// - A "source" node, which is only emanates flow outward
-    /// - A "sink" node, which only receives flow
-    public struct FlowNetwork <Value: Hashable>: Hashable {
+    /// - Returns: All of the `Node` values contained herein which are neither the `source` nor
+    /// the `sink`.
+    public var internalNodes: [Node] {
+        return graph.nodes.filter { $0 != source && $0 != sink }
+    }
 
-        public typealias Path = Graph<Value>.Path
-        public typealias Node = Graph<Value>.Node
+    /// - Returns: All of the paths from the `source` to the `sink`.
+    internal var paths: Set<Path> {
+        return graph.paths(from: source, to: sink)
+    }
 
-        /// - Returns: All of the `Node` values contained herein which are neither the `source` nor
-        /// the `sink`.
-        public var internalNodes: [Node] {
-            return graph.nodes.filter { $0 != source && $0 != sink }
+    // TODO: Consider more (space-)efficient storage of Nodes.
+    internal var graph: Graph<Value>
+    internal var source: Node
+    internal var sink: Node
+
+    // MARK: - Initializers
+
+    /// Create a `FlowNetwork` with the given `graph` and the given `source` and `sink` nodes.
+    public init(_ graph: Graph<Value>, source: Graph<Value>.Node, sink: Graph<Value>.Node) {
+        self.graph = graph
+        self.source = source
+        self.sink = sink
+    }
+
+    /// - TODO: Implement
+    private func edmondsKarp() {
+
+        // Create Residual Network
+        var residual = graph
+
+        // Iterate over all Augmenting Paths
+
+        while let path = residual.shortestPath(from: source, to: sink) {
+            // Get maximum flow of shortest path:
+            let maximumFlow = path.map { $0.value }.min()!
+
         }
 
-        /// - Returns: All of the paths from the `source` to the `sink`.
-        internal var paths: Set<Path> {
-            return graph.paths(from: source, to: sink)
-        }
 
-        // TODO: Consider more (space-)efficient storage of Nodes.
-        internal var graph: Graph<Value>
-        internal var source: Node
-        internal var sink: Node
+        // ...
 
-        // MARK: - Initializers
+        // just keep puuuuushin' it through
 
-        /// Create a `FlowNetwork` with the given `graph` and the given `source` and `sink` nodes.
-        public init(_ graph: Graph<Value>, source: Graph<Value>.Node, sink: Graph<Value>.Node) {
-            self.graph = graph
-            self.source = source
-            self.sink = sink
-        }
-
-        /// - TODO: Implement
-        private func edmondsKarp() {
-
-            // Create Residual Network
-            var residual = graph
-
-            // Iterate over all Augmenting Paths
-
-            while let path = residual.shortestPath(from: source, to: sink) {
-                // Get maximum flow of shortest path:
-                let maximumFlow = path.map { $0.value }.min()!
-
-            }
-
-
-            // ...
-
-            // just keep puuuuushin' it through
-
-            // ...
-        }
+        // ...
     }
 }
