653ebef7eb6a517b44f173b1991b6389efb6957f
James <jbean46@gmail.com>
2018-06-10 14:23:24 -0700

    Organize

---
+++ Sources/PitchSpeller/ModifierDirection.swift
@@ -0,0 +1,14 @@
+//
+//  ModifierDirection.swift
+//  PitchSpeller
+//
+//  Created by James Bean on 6/10/18.
+//
+
+/// The possible directions of `QuarterStepModifier` values within the context of
+/// a `PitchSpellingCategoryProtocol`.
+enum ModifierDirection: Hashable {
+    case down
+    case neutral
+    case up
+}
