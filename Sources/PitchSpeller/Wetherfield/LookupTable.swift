a86de9cfa9ade6208e573cdce330c4c7d7e550f2
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-26 16:57:15 +0100

    implement lookup table for underlying edge values

--- Sources/PitchSpeller/Wetherfield/LookupTable.swift
+++ Sources/PitchSpeller/Wetherfield/LookupTable.swift
@@ -23,3 +23,32 @@ struct LookupIndex: Hashable {
         self.tendency = tuple.1
     }
 }
+
+struct LookupPair: Hashable {
+    
+    let source: LookupIndex
+    let sink: LookupIndex
+    
+    init(_ source: LookupIndex, _ sink: LookupIndex) {
+        self.source = source
+        self.sink = sink
+    }
+    
+    init(_ tuple: (LookupIndex, LookupIndex)) {
+        self.source = tuple.0
+        self.sink = tuple.1
+    }
+}
+
+struct LookupTable<Weight: Hashable & Numeric> {
+    
+    private let storage: [LookupPair: Weight]
+    
+    init(_ dict: [LookupPair: Weight]) {
+        self.storage = dict
+    }
+    
+    func weight(at index: LookupPair) -> Weight? {
+        return storage[index]
+    }
+}
