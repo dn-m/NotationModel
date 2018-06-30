25985d3d209d4938f980d6004551bdba13541aa1
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-01 00:08:05 +0100

    Add passing test of Equality function for `Cross`

--- Tests/PitchSpellerTests/CrossTests.swift
+++ Tests/PitchSpellerTests/CrossTests.swift
@@ -10,4 +10,11 @@ import XCTest
 
 class CrossTests: XCTestCase {
     
+    func testEqualityFunction() {
+        let pair1 = Cross<Int, String>(3, "three")
+        let pair2 = Cross<Int, String>(3, "three")
+        let pair3 = Cross<Int, String>(3, "four")
+        XCTAssertEqual(pair1, pair2)
+        XCTAssertNotEqual(pair1, pair3)
+    }
 }
