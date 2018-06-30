269fea62cf17229e199c470d88fddd3cd480ee8f
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 23:50:12 +0100

    Add passing test of `OrderedPair` equality function

--- Tests/PitchSpellerTests/OrderedPairTests.swift
+++ Tests/PitchSpellerTests/OrderedPairTests.swift
@@ -10,4 +10,11 @@ import XCTest
 
 class OrderedPairTests: XCTestCase {
     
+    func testEqualityFunction() {
+        let pair1 = OrderedPair<Int>(1,2)
+        let pair2 = OrderedPair<Int>(1,2)
+        let pair3 = OrderedPair<Int>(2,1)
+        XCTAssertEqual(pair1, pair2)
+        XCTAssertNotEqual(pair1, pair3)
+    }
 }
