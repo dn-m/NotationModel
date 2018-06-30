2665029a2e253ec8ab4a3163de31bc6fbcdcbeed
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 23:53:52 +0100

    Add passing Hashability test for `OrderedPair`

--- Tests/PitchSpellerTests/OrderedPairTests.swift
+++ Tests/PitchSpellerTests/OrderedPairTests.swift
@@ -17,4 +17,12 @@ class OrderedPairTests: XCTestCase {
         XCTAssertEqual(pair1, pair2)
         XCTAssertNotEqual(pair1, pair3)
     }
+    
+    func testHashability() {
+        let pair = OrderedPair<Int>(2,4)
+        let riap = OrderedPair<Int>(4,2)
+        let dict = [pair: "Hash Value"]
+        XCTAssertEqual(dict[pair], "Hash Value")
+        XCTAssertNil(dict[riap])
+    }
 }
