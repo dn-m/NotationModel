f16a25950224c53e4e6a46d10f8b47ee477dcedf
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 18:53:49 +0100

    Add `Cross` minimal implementation
    Name suggests "Cross Product"

--- Sources/PitchSpeller/Data Structures/Cross.swift
+++ Sources/PitchSpeller/Data Structures/Cross.swift
@@ -5,3 +5,21 @@
 //  Created by Benjamin Wetherfield on 6/30/18.
 //
 
+struct Cross<T,U>: Pairable {
+    
+    typealias A = T
+    typealias B = U
+    
+    let a: T
+    let b: U
+    
+    init(_ a: A, _ b: B) {
+        self.a = a
+        self.b = b
+    }
+    
+    init(_ pair: (A, B)) {
+        self.a = pair.0
+        self.b = pair.1
+    }
+}
