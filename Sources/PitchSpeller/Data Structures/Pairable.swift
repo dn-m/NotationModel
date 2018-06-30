0cded4632f6fc57c816054bd7105bff75bf3ec7f
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 04:24:20 -0700

    Implement `Pairable`

--- Sources/PitchSpeller/Data Structures/Pairable.swift
+++ Sources/PitchSpeller/Data Structures/Pairable.swift
@@ -4,3 +4,13 @@
 //
 //  Created by Benjamin Wetherfield on 6/30/18.
 //
+
+protocol Pairable {
+    associatedtype A
+    associatedtype B
+    var a: A { get }
+    var b: B { get }
+    var tuple: (A,B) { get }
+    init(_ pair: (A,B))
+    init(_ a: A, _ b: B)
+}
