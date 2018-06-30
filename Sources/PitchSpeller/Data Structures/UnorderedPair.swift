a8f8e0c55ec7512b4dcf184afb88af8d9df088c7
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 04:40:57 -0700

    Implement basic UnorderedPair

--- Sources/PitchSpeller/Data Structures/UnorderedPair.swift
+++ Sources/PitchSpeller/Data Structures/UnorderedPair.swift
@@ -5,3 +5,20 @@
 //  Created by Benjamin Wetherfield on 6/30/18.
 //
 
+struct UnorderedPair<T>: Symmetric {
+    
+    typealias A = T
+    
+    let a: T
+    let b: T
+    
+    init(_ pair: (T, T)) {
+        self.a = pair.0
+        self.b = pair.1
+    }
+    
+    init(_ a: T, _ b: T) {
+        self.a = a
+        self.b = b
+    }
+}
