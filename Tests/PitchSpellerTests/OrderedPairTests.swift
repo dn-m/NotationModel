cf28de9cd1abf1acdef1c4128721cc489fc18d00
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 23:58:47 +0100

    Add passing test for `elput` property

--- Tests/PitchSpellerTests/OrderedPairTests.swift
+++ Tests/PitchSpellerTests/OrderedPairTests.swift
@@ -31,4 +31,10 @@ class OrderedPairTests: XCTestCase {
         XCTAssertEqual(pair.tuple.0, 3)
         XCTAssertEqual(pair.tuple.1, 6)
     }
+    
+    func testElput() {
+        let pair = OrderedPair<Int>(3,6)
+        XCTAssertEqual(pair.elput.0, 6)
+        XCTAssertEqual(pair.elput.1, 3)
+    }
 }
