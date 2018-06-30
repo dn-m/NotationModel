fbe38220f6372417441798c0c558033afc967fac
Ben Wetherfield <bwetherfield@gmail.com>
2018-06-30 04:32:53 -0700

    Add default implementation of `elput` for `Swappable`

--- Sources/PitchSpeller/Data Structures/Swappable.swift
+++ Sources/PitchSpeller/Data Structures/Swappable.swift
@@ -9,3 +9,7 @@ protocol Swappable: Symmetric {
     var elput: (A,A) { get }
     var swapped: Self { get }
 }
+
+extension Swappable {
+    var elput: (A,A) { get { return (tuple.1, tuple.0) } }
+}
