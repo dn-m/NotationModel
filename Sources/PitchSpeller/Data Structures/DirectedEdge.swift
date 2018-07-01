e7637cc1ab5f48c0b596b8513ac5a610b0cdf625
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-01 05:45:16 +0100

    Add properties to `DirectedEdge`

--- Sources/PitchSpeller/Data Structures/DirectedEdge.swift
+++ Sources/PitchSpeller/Data Structures/DirectedEdge.swift
@@ -10,5 +10,6 @@ public struct DirectedEdge<Node, Num>: Weighted & NodePair {
     typealias Weight = Num
     typealias Pair = OrderedPair<Node>
 
-    
+    var weight: Weight
+    let nodes: Pair
 }
