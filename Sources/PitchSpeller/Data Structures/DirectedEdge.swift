24dace50974b721e077e40b14b2a4be199dfad9e
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-01 05:44:22 +0100

    Add internal typedefs to `DirectedEdge`

--- Sources/PitchSpeller/Data Structures/DirectedEdge.swift
+++ Sources/PitchSpeller/Data Structures/DirectedEdge.swift
@@ -5,6 +5,10 @@
 //  Created by Benjamin Wetherfield on 7/1/18.
 //
 
-public struct DirectedEdge: Weighted & NodePair {
+public struct DirectedEdge<Node, Num>: Weighted & NodePair {
+
+    typealias Weight = Num
+    typealias Pair = OrderedPair<Node>
+
     
 }
