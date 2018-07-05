b7c74c525767731d11cbcd1300a050290c3987e1
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-05 04:39:03 +0100

    Remove `source` and `destination` from `NodePair`
    Pare down to essentials

--- Sources/PitchSpeller/Data Structures/NodePair.swift
+++ Sources/PitchSpeller/Data Structures/NodePair.swift
@@ -8,11 +8,4 @@
 protocol NodePair {
     associatedtype Pair: Symmetric
     var nodes: Pair { get }
-    var source: Pair.A { get }
-    var destination: Pair.A { get }
-}
-
-extension NodePair {
-    var source: Pair.A { return nodes.a }
-    var destination: Pair.A { return nodes.b }
 }
