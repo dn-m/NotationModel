882b8045da0127fabb812e68b026a3f34fe3ce47
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-05 03:56:15 +0100

    Leave line in doc comments

--- Sources/PitchSpeller/Data Structures/Pairable.swift
+++ Sources/PitchSpeller/Data Structures/Pairable.swift
@@ -8,13 +8,16 @@
 protocol Pairable {
     
     // MARK: - Associated Types
+    
     associatedtype A
     associatedtype B
     
     // MARK: - Instance Properties
+    
     var a: A { get }
     var b: B { get }
     
     // MARK: - Initializers
+    
     init(_ a: A, _ b: B)
 }
