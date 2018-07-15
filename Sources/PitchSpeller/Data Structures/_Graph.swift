75c4c1ba8fb939fe985eabcaa99837736ec6fa81
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-15 20:25:54 +0100

    Refactor computation of edges to use Pair-based initializer

--- Sources/PitchSpeller/Data Structures/_Graph.swift
+++ Sources/PitchSpeller/Data Structures/_Graph.swift
@@ -60,7 +60,7 @@ struct _Graph<Weight: Weightedness, Pair: SymmetricPair & Directedness & Hashabl
     var adjacents: [Pair: Weight]
     
     var edges: [Edge] {
-        return adjacents.compactMap{ Edge($0.key.a, $0.key.b, withWeight: $0.value) }
+        return adjacents.compactMap{ Edge($0.key, withWeight: $0.value) }
     }
     
     // MARK: - Instance Methods
