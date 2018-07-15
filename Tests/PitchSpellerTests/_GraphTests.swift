16beb05d1d6fabc876a46e389b9122fb1b6d2957
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-15 19:28:33 +0100

    Fix argument label for insertEdge

--- Tests/PitchSpellerTests/_GraphTests.swift
+++ Tests/PitchSpellerTests/_GraphTests.swift
@@ -44,7 +44,7 @@ class _GraphTests: XCTestCase {
         var graph = _Graph<Double, DirectedOver<Int> >()
         graph.insertNode(0)
         graph.insertNode(1)
-        graph.insertEdge(from: 0, to: 1, value: 0.5)
+        graph.insertEdge(from: 0, to: 1, withWeight: 0.5)
         XCTAssertEqual(graph.edgeValue(from: 0, to: 1), 0.5)
     }
 //
