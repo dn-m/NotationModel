15b407cbace6e372e09f27c7cf8eff66893a5240
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-15 18:41:18 -0700

    Add symmetric tests for undirected case of edges from node

--- Tests/PitchSpellerTests/_GraphTests.swift
+++ Tests/PitchSpellerTests/_GraphTests.swift
@@ -99,8 +99,11 @@ class _GraphTests: XCTestCase {
         graph.insertNode("c")
         graph.insertEdge(from: "a", to: "c", withWeight: 0.5)
         let edgesFromA = graph.edges(from: "a")
+        let edgesFromC = graph.edges(from: "a")
         XCTAssertEqual(edgesFromA[0].weight, 0.5)
         XCTAssertEqual(edgesFromA.count, 1)
+        XCTAssertEqual(edgesFromC[0].weight, 0.5)
+        XCTAssertEqual(edgesFromC.count, 1)
     }
 //
     func testNodesAdjacentToNodeDirected() {
