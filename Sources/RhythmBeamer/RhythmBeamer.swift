68e0d1bfbc535edd06cf35bb74a9b2082031ccf6
James <jbean46@gmail.com>
2018-07-24 00:34:17 -0700

    Make beamingVerticals(_:) a one-liner)

--- Sources/RhythmBeamer/RhythmBeamer.swift
+++ Sources/RhythmBeamer/RhythmBeamer.swift
@@ -6,6 +6,7 @@
 //
 
 import DataStructures
+import Algorithms
 import Math
 import MetricalDuration
 import Rhythm
@@ -20,6 +21,23 @@ public enum DefaultBeamer {
     }
 }
 
+extension Beaming {
+    /// Create a `Beaming` with the given amount of beams per vertical.
+    init(beamCounts: [Int]) {
+        self.init(sanitizingBeamletDirections(for: beamingVerticals(beamCounts)))
+    }
+}
+
+/// - Returns: An array of `BeamJunction` values for the given `leaves`.
+func beamingVerticals (_ leaves: [MetricalDuration]) -> [Beaming.Point.Vertical] {
+    return beamingVerticals(leaves.map(beamCount))
+}
+
+/// - Returns: An array of `Point.Vertical` values for the given `counts` (amounts of beams).
+func beamingVerticals (_ counts: [Int]) -> [Beaming.Point.Vertical] {
+    return counts.indices.map { map($0-1,$0,$0+1) { counts[safe: $0] ?? 0 } }.map(vertical)
+}
+
 /// - Returns: a `Beaming.Point.Vertical` with the given context:
 ///
 /// - prev: Previous beaming count (if it exists, or 0 if it doesn't)
@@ -29,42 +47,12 @@ public enum DefaultBeamer {
 /// - Note: This is wrapped up in its own function to avoid having to disambiquate `min` and `max`
 /// for `Sequence` types and the free functions. Otherwise, `Swift.` must be prepended to each
 /// instance, which is pretty gross.
-private func vertical(_ prev: Int, _ cur: Int, _ next: Int) -> Beaming.Point.Vertical {
+func vertical(_ prev: Int, _ cur: Int, _ next: Int) -> Beaming.Point.Vertical {
     return .init(
         maintain: min(prev,cur,next),
         startOrStop: .init(start: max(0,min(cur,next)-prev), stop: max(0,min(cur,prev)-next)),
         beamlets: max(0,cur-max(prev,next))
     )
-}
-
-extension Beaming.Point.StartOrStop {
-    /// Create a `StartOrStop` with the given amounts. You can't start and stop in the same
-    /// `vertical`, so don't.
-    init(start: Int, stop: Int) {
-        self = start > 0 ? .start(count: start) : stop > 0 ? .stop(count: stop) : .none
-    }
-}
-
-extension Beaming {
-    /// Create a `Beaming` with the given amount of beams per vertical.
-    init(beamCounts: [Int]) {
-        self.init(sanitizingBeamletDirections(for: beamingVerticals(beamCounts)))
-    }
-}
-
-/// - Returns: An array of `Point.Vertical` values for the given `counts` (amounts of beams).
-func beamingVerticals (_ counts: [Int]) -> [Beaming.Point.Vertical] {
-    return counts.indices.map { index in
-        let prev = counts[safe: index - 1] ?? 0
-        let cur = counts[index]
-        let next = counts[safe: index + 1] ?? 0
-        return vertical(prev,cur,next)
-    }
-}
-
-/// - Returns: An array of `BeamJunction` values for the given `leaves`.
-func beamingVerticals (_ leaves: [MetricalDuration]) -> [Beaming.Point.Vertical] {
-    return beamingVerticals(leaves.map(beamCount))
 }
 
 /// - Returns: Amount of beams needed to represent the given `duration`.
@@ -81,4 +69,12 @@ func beamCount(_ duration: MetricalDuration) -> Int {
         }
     }
     fatalError("\(duration) is not representable with beams")
+}
+
+extension Beaming.Point.StartOrStop {
+    /// Create a `StartOrStop` with the given amounts. You can't start and stop in the same
+    /// `vertical`, so don't.
+    init(start: Int, stop: Int) {
+        self = start > 0 ? .start(count: start) : stop > 0 ? .stop(count: stop) : .none
+    }
 }
