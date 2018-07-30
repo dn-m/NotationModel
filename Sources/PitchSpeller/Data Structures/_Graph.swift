8bc1eb7fa33b4b790d4f9f42f77d5c98b0e3ae16
Ben Wetherfield <bwetherfield@gmail.com>
2018-07-29 23:17:24 -0400

    Add comment to high level neighbors function

--- Sources/PitchSpeller/Data Structures/_Graph.swift
+++ Sources/PitchSpeller/Data Structures/_Graph.swift
@@ -132,6 +132,7 @@ struct _Graph<Weight: Weightedness, Pair: SymmetricPair & Directedness & Hashabl
         return nodes.filter { adjacents.keys.contains(Pair(source, $0)) }
     }
     
+    /// - Returns: Array of nodes adjacent to `source` out of the supplied array of `nodes`.
     func neighbors (of source: Node, from nodes: [Node]) -> [Node] {
         return neighbors(of: source, from: Set(nodes))
     }
