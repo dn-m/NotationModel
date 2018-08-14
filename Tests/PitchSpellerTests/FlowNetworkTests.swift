11b08425884e59588d43599315ec9306d1b85316
Ben Wetherfield <bwetherfield@gmail.com>
2018-08-14 08:35:47 +0100

    Replace unwrapping with compactMap

--- Tests/PitchSpellerTests/FlowNetworkTests.swift
+++ Tests/PitchSpellerTests/FlowNetworkTests.swift
@@ -28,7 +28,8 @@ class FlowNetworkTests: XCTestCase {
         let diGraph = flowNetwork.directedGraph
         let cutValue = minCut.0.lazy.map { source in
             return diGraph.neighbors(of: source, from: minCut.1).lazy
-                .reduce(0.0, { $0 + diGraph.weight(from: source, to: $1)! })
+                .compactMap { diGraph.weight(from: source, to: $0) }
+                .reduce(0.0, { $0 + $1 })
         }
         .reduce(0.0, { $0 + $1 })
         XCTAssertEqual(cutValue, flowNetwork.solvedForMaximumFlow.flow)
