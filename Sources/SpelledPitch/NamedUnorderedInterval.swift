3a3dc162c602c7fb255ca785109f9d3407d1e35b
James <jbean46@gmail.com>
2018-08-17 16:47:44 -0400

    Restructure NamedUnorderedInterval for organization and docs

--- Sources/SpelledPitch/NamedUnorderedInterval.swift
+++ Sources/SpelledPitch/NamedUnorderedInterval.swift
@@ -14,48 +14,107 @@ import Pitch
 /// `SpelledPitch` values.
 public struct NamedUnorderedInterval {
 
+    // MARK: - Instance Properties
+
+    /// Quality value of a `NamedUnorderedInterval`.
+    ///
+    /// - `diminished`
+    /// - `minor`
+    /// - `perfect`
+    /// - `major`
+    /// - `augmented`
+    ///
+    public let quality: Quality
+
+    /// Ordinal value of a `NamedUnorderedInterval` (
+    ///
+    /// - `unison`
+    /// - `second`
+    /// - `third`
+    /// - `fourth`
+    ///
+    public let ordinal: Ordinal
+}
+
+extension NamedUnorderedInterval {
+
     // MARK: - Associated Types
-    
+
     /// `PitchType` with level of ordering necessary to construct a `NamedUnorderedInterval`.
     public typealias PitchType = Pitch.Class
-    
+
     /// Type describing the quality of a `NamedInterval`-conforming type.
     public typealias Quality = NamedIntervalQuality
-    
+}
+
+extension NamedUnorderedInterval {
+
     // MARK: - Nested Types
 
     /// The ordinal of a `NamedUnorderedInterval`.
     public enum Ordinal {
 
-        /// Perfect ordinals.
-        public enum Perfect {
-            case unison, fourth
-        }
+        // MARK: - Cases
 
-        /// Imperfect ordinals.
-        public enum Imperfect {
-            case second, third
-        }
-
+        /// Imperfect named unordered interval ordinals (unison, fourth).
         case perfect(Perfect)
-        case imperfect(Imperfect)
 
-        /// Creates a `NamedUnorderedInterval` with the given amount of `steps`.
-        public init?(steps: Int) {
-            switch steps {
-            case 0:
-                self = .perfect(.unison)
-            case 1:
-                self = .imperfect(.second)
-            case 2:
-                self = .imperfect(.third)
-            case 3:
-                self = .perfect(.fourth)
-            default:
-                return nil
-            }
+        /// Perfect named unordered interval ordinals (second, third).
+        case imperfect(Imperfect)
+    }
+}
+
+extension NamedUnorderedInterval.Ordinal {
+
+    // MARK: - Initializers
+    
+    /// Creates a `NamedUnorderedInterval` with the given amount of `steps`.
+    public init?(steps: Int) {
+        switch steps {
+        case 0:
+            self = .perfect(.unison)
+        case 1:
+            self = .imperfect(.second)
+        case 2:
+            self = .imperfect(.third)
+        case 3:
+            self = .perfect(.fourth)
+        default:
+            return nil
         }
     }
+}
+
+extension NamedUnorderedInterval.Ordinal {
+
+    // MARK: - Nested Types
+
+    /// Perfect ordinals.
+    public enum Perfect {
+
+        // MARK: - Cases
+
+        /// Unison perfect named unordered interval ordinal.
+        case unison
+
+        /// Fourth perfect named unordered interval ordinal.
+        case fourth
+    }
+
+    /// Imperfect ordinals.
+    public enum Imperfect {
+
+        // MARK: - Cases
+
+        /// Second imperfect named unordered interval ordinal.
+        case second
+
+        /// Third imperfect named unordered interval ordinal.
+        case third
+    }
+}
+
+extension NamedUnorderedInterval {
 
     // MARK: - Type Properties
 
@@ -63,16 +122,10 @@ public struct NamedUnorderedInterval {
     public static var unison: NamedUnorderedInterval {
         return .init(.perfect, .unison)
     }
-    
-    // MARK: - Instance Properties
-    
-    /// Ordinal value of a `NamedUnorderedInterval` (`unison`, `second`, `third`, `fourth`).
-    public let ordinal: Ordinal
-    
-    /// Quality value of a `NamedUnorderedInterval`
-    /// (`diminished`, `minor`, `perfect`, `major`, `augmented`).
-    public let quality: Quality
-    
+}
+
+extension NamedUnorderedInterval {
+
     // MARK: - Initializers
 
     /// Create a perfect `NamedUnorderedInterval`.
@@ -155,10 +208,10 @@ public struct NamedUnorderedInterval {
         self.quality = quality
         self.ordinal = ordinal
     }
-    
+
     /// Create a `NamedUnorderedInterval` with two `SpelledPitch` values.
     public init(_ a: Pitch.Spelling, _ b: Pitch.Spelling) {
-        
+
         // Ensure that the two `Pitch.Speller` values are in the correct order to create
         // a relative interval.
         let (a,b) = ordered(a,b)
@@ -167,14 +220,14 @@ public struct NamedUnorderedInterval {
         // the `Pitch.Spelling.letterName` properties, and the difference between the
         // `noteNumber` properties.
         let (steps, interval) = stepsAndInterval(a,b)
-        
+
         // Sanitize interval in order to calulate the `Quality`.
         let sanitizedInterval = sanitizeIntervalClass(interval, steps: steps)
-        
+
         // Create the necessary structures
         let ordinal = Ordinal(steps: steps)!
         let quality = Quality(sanitizedIntervalClass: sanitizedInterval, ordinal: ordinal)
-        
+
         // Init
         self.init(quality, ordinal)
     }
@@ -183,19 +236,19 @@ public struct NamedUnorderedInterval {
 extension NamedUnorderedInterval.Ordinal: Equatable, Hashable { }
 extension NamedUnorderedInterval: Equatable, Hashable { }
 
-private func ordered (_ a: Pitch.Spelling, _ b: Pitch.Spelling)
-    -> (Pitch.Spelling, Pitch.Spelling)
-{
+/// - Returns: The two `Pitch.Spelling` values such that the difference between `b` and `a` is less
+/// that the difference between `a` and `b`.
+private func ordered (_ a: Pitch.Spelling, _ b: Pitch.Spelling) -> (Pitch.Spelling, Pitch.Spelling) {
     let (a,b,_) = swapped(a, b) { mod(steps(a,b), 7) > mod(steps(b,a), 7) }
     return (a,b)
 }
 
-private func stepsAndInterval(_ a: Pitch.Spelling, _ b: Pitch.Spelling)
-    -> (Int, Double)
-{
+/// - Returns: The steps and interval between the two given `Pitch.Spelling` values.
+private func stepsAndInterval(_ a: Pitch.Spelling, _ b: Pitch.Spelling) -> (Int, Double) {
     return (steps(a,b), interval(a,b))
 }
 
+/// - Returns: Sanitizes the interval class with the given `steps`.
 private func sanitizeIntervalClass(_ intervalClass: Double, steps: Int) -> Double {
 
     // 1. Retrieve the platonic ideal interval class (neutral second, neutral third)
