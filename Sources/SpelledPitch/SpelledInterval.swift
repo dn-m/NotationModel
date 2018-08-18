697b023e3533c235819b1f95f6537daa2488ae0a
James <jbean46@gmail.com>
2018-08-18 10:49:20 -0400

    Make platonic distance a local init rather than func

--- Sources/SpelledPitch/SpelledInterval.swift
+++ Sources/SpelledPitch/SpelledInterval.swift
@@ -39,41 +39,76 @@ extension SpelledInterval {
     {
         let distance = Ordinal.platonicDistance(from: interval, to: steps)
         let ordinal = Ordinal(steps: steps)!
-        let threshold = ordinal.platonicThreshold
-        return (quality(distance: distance, with: threshold), ordinal)
+        let quality = SpelledIntervalQuality(distance: distance, with: ordinal.platonicThreshold)
+        return (quality, ordinal)
     }
 }
 
-private func quality(distance: Double, with platonicThreshold: Double) -> SpelledIntervalQuality {
-    let (diminished, augmented) = (-platonicThreshold,platonicThreshold)
-    switch distance {
-    case diminished - 4:
-        return .extended(.init(.quintuple, .diminished))
-    case diminished - 3:
-        return .extended(.init(.quadruple, .diminished))
-    case diminished - 2:
-        return .extended(.init(.triple, .diminished))
-    case diminished - 1:
-        return .extended(.init(.double, .diminished))
-    case diminished:
-        return .extended(.init(.single, .diminished))
-    case -0.5:
-        return .imperfect(.minor)
-    case +0.0:
-        return .perfect(.perfect)
-    case +0.5:
-        return .imperfect(.major)
-    case augmented:
-        return .extended(.init(.single, .augmented))
-    case augmented + 1:
-        return .extended(.init(.double, .augmented))
-    case augmented + 2:
-        return .extended(.init(.triple, .augmented))
-    case augmented + 3:
-        return .extended(.init(.quadruple, .augmented))
-    case augmented + 4:
-        return .extended(.init(.quintuple, .augmented))
-    default:
-        fatalError("Not possible to create a NamedIntervalQuality with interval \(distance)")
+extension SpelledIntervalQuality {
+    init(distance: Double, with platonicThreshold: Double) {
+        let (diminished, augmented) = (-platonicThreshold,platonicThreshold)
+        switch distance {
+        case diminished - 4:
+            self =  .extended(.init(.quintuple, .diminished))
+        case diminished - 3:
+            self = .extended(.init(.quadruple, .diminished))
+        case diminished - 2:
+            self = .extended(.init(.triple, .diminished))
+        case diminished - 1:
+            self = .extended(.init(.double, .diminished))
+        case diminished:
+            self = .extended(.init(.single, .diminished))
+        case -0.5:
+            self = .imperfect(.minor)
+        case +0.0:
+            self = .perfect(.perfect)
+        case +0.5:
+            self = .imperfect(.major)
+        case augmented:
+            self = .extended(.init(.single, .augmented))
+        case augmented + 1:
+            self = .extended(.init(.double, .augmented))
+        case augmented + 2:
+            self = .extended(.init(.triple, .augmented))
+        case augmented + 3:
+            self = .extended(.init(.quadruple, .augmented))
+        case augmented + 4:
+            self = .extended(.init(.quintuple, .augmented))
+        default:
+            fatalError("Not possible to create a NamedIntervalQuality with interval \(distance)")
+        }
     }
 }
+//private func quality(distance: Double, with platonicThreshold: Double) -> SpelledIntervalQuality {
+//    let (diminished, augmented) = (-platonicThreshold,platonicThreshold)
+//    switch distance {
+//    case diminished - 4:
+//        return .extended(.init(.quintuple, .diminished))
+//    case diminished - 3:
+//        return .extended(.init(.quadruple, .diminished))
+//    case diminished - 2:
+//        return .extended(.init(.triple, .diminished))
+//    case diminished - 1:
+//        return .extended(.init(.double, .diminished))
+//    case diminished:
+//        return .extended(.init(.single, .diminished))
+//    case -0.5:
+//        return .imperfect(.minor)
+//    case +0.0:
+//        return .perfect(.perfect)
+//    case +0.5:
+//        return .imperfect(.major)
+//    case augmented:
+//        return .extended(.init(.single, .augmented))
+//    case augmented + 1:
+//        return .extended(.init(.double, .augmented))
+//    case augmented + 2:
+//        return .extended(.init(.triple, .augmented))
+//    case augmented + 3:
+//        return .extended(.init(.quadruple, .augmented))
+//    case augmented + 4:
+//        return .extended(.init(.quintuple, .augmented))
+//    default:
+//        fatalError("Not possible to create a NamedIntervalQuality with interval \(distance)")
+//    }
+//}
