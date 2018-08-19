4308226a7a0021e36769d3c9655881ce64ba5def
James <jbean46@gmail.com>
2018-08-18 21:10:52 -0400

    Reuse reachAroundAdjustment, add error, remove dead code

--- Sources/SpelledPitch/PitchSpeller/Pitch+Pitch.Spelling.swift
+++ Sources/SpelledPitch/PitchSpeller/Pitch+Pitch.Spelling.swift
@@ -16,6 +16,10 @@ import Pitch
 
 extension Pitch {
 
+    public enum Error: Swift.Error {
+        case incompatibleSpelling(Pitch.Spelling<EDO48>,Pitch)
+    }
+
     /// All `Pitch.Spelling` structures available for this `Pitch`.
     public var spellings: [Pitch.Spelling<EDO48>] {
         return self.class.spellings
@@ -33,39 +37,10 @@ extension Pitch {
     /// not appropriate for this `Pitch`.
     ///
     public func spelled(with spelling: Pitch.Spelling<EDO48>) throws -> SpelledPitch<EDO48> {
-
-        var octave: Int {
-
-            let unadjusted = Int(floor(noteNumber.value / 12.0)) - 1
-
-            var mustAdjustForC: Bool {
-                guard spelling.letterName == .c else { return false }
-                if spelling.modifier.edo24.adjustment < 0 { return false }
-                if case .natural = spelling.modifier.edo24.edo12, spelling.modifier.adjustment < 0 {
-                    return true
-                } else {
-                    return false
-                }
-
-//                guard spelling.letterName == .c else { return false }
-//                if spelling.quarterStep.direction == .down { return true }
-//                return spelling.quarterStep == .natural && spelling.eighthStep == .down
-            }
-
-            var mustAdjustForB: Bool {
-                guard spelling.letterName == .b else { return false }
-                if case .sharp = spelling.modifier.edo24.edo12, spelling.modifier.adjustment >= 0 {
-                    return true
-                } else {
-                    return false
-                }
-//                return spelling.quarterStep == .sharp && spelling.eighthStep.rawValue >= 0
-            }
-//
-            return mustAdjustForC ? unadjusted + 1 : mustAdjustForB ? unadjusted - 1 : unadjusted
+        guard spelling.pitchClass == self.class else {
+            throw Error.incompatibleSpelling(spelling, self)
         }
-
-
+        let octave = Int(floor(noteNumber.value / 12.0)) - 1 - reachAroundAdjustment(for: spelling)
         return SpelledPitch(spelling, octave)
     }
 
