b437453ce727972be3c7aab01a115d6e16a044f8
James <jbean46@gmail.com>
2018-08-18 22:55:22 -0400

    Remove duplicated protocol property

--- Sources/SpelledPitch/EDO/EDO.swift
+++ Sources/SpelledPitch/EDO/EDO.swift
@@ -7,12 +7,3 @@
 
 /// Interface for the `EDO` (equal divisions of the octave) `TuningSystem` types.
 public protocol EDO: TuningSystem { }
-
-/// Interface for `Modifier` types within an `EDO` `TuningSystem`.
-protocol EDOModifier {
-
-    // MARK: - Static Properties
-
-    /// A `Modifier` which does not apply any modification.
-    static var identity: Self { get }
-}
