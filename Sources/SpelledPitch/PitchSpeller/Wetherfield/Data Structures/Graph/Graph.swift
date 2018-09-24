5012286c5669f1f7f6d66ddabb1b975405fd35bf
James <jbean46@gmail.com>
2018-09-24 16:16:34 -0400

    Add doc comments for Graph

--- Sources/SpelledPitch/PitchSpeller/Wetherfield/Data Structures/Graph/Graph.swift
+++ Sources/SpelledPitch/PitchSpeller/Wetherfield/Data Structures/Graph/Graph.swift
@@ -7,19 +7,44 @@
 
 /// Unweighted, undirected graph.
 struct Graph <Node: Hashable>: UnweightedGraphProtocol, UndirectedGraphProtocol {
-    typealias Edge = UnorderedPair<Node>
-    var nodes: Set<Node>
-    var edges: Set<Edge>
 
+    // MARK: - Instance Properties
+
+    /// All of the nodes contained herein.
+    ///
+    /// A `Node` is any `Hashable`-conforming value.
+    var nodes: Set<Node>
+
+    /// All of the edges contained herein.
+    ///
+    /// An `Edge` is an `UnorderedPair` of `Node` values.
+    var edges: Set<Edge>
+}
+
+extension Graph {
+
+    // MARK: - Initializers
+
+    /// Creates a `Graph` with the given set of nodes, with no edges between the nodes.
     init(_ nodes: Set<Node> = []) {
         self.nodes = nodes
         self.edges = []
     }
-    
+
+    /// Creates a `Graph` with the given set of nodes and the given set of edges connecting the
+    /// nodes.
     init(_ nodes: Set<Node> = [], _ edges: Set<Edge> = []) {
         self.init(nodes)
         self.edges = edges
     }
+}
+
+extension Graph {
+
+    // MARK: - Type Aliases
+
+    /// The type of edges which connect nodes.
+    typealias Edge = UnorderedPair<Node>
 }
 
 extension Graph: Equatable { }
