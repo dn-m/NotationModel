2adbe1fd4c4b4c3df28e2cb3780fa57443218918
James <jbean46@gmail.com>
2018-10-18 11:24:52 -0400

    Remove superfluous Pitch.Spelling.init

--- Sources/SpelledPitch/Pitch.Spelling.swift
+++ Sources/SpelledPitch/Pitch.Spelling.swift
@@ -46,16 +46,6 @@ extension Pitch.Spelling where Tuning == EDO12 {
     /// **Example Usage**
     ///
     ///     let cNatural = Pitch.Spelling(.c)
-    ///
-    public init(_ letterName: LetterName) {
-        self.init(letterName: letterName, modifier: .natural)
-    }
-
-    /// Creates a `Pitch.Spelling`.
-    ///
-    /// **Example Usage**
-    ///
-    ///     let cNatural = Pitch.Spelling(.c)
     ///     let aFlat = Pitch.Spelling(.a, .flat)
     ///
     public init(_ letterName: LetterName, _ modifier: EDO12.Modifier = .natural) {
