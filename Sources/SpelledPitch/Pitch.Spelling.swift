be9f3e86d735d6eab0c4dffcaa902fbb42befc36
James <jbean46@gmail.com>
2018-10-20 11:17:21 -0400

    Create new Spelling base on Helmholtz-Ellis system

--- Sources/SpelledPitch/Pitch.Spelling.swift
+++ Sources/SpelledPitch/Pitch.Spelling.swift
@@ -8,6 +8,96 @@
 
 import Pitch
 
+struct Spelling {
+
+    /// The modification of a `Pitch.Spelling` from the given `LetterName`.
+    struct Modifier {
+
+        // MARK: - Nested Types
+
+        /// Base modifier (natural, sharps, flats).
+        struct Pythagorean {
+
+            // MARK: - Type Properties
+
+            static let doubleFlat = Pythagorean(-2)
+            static let flat = Pythagorean(-1)
+            static let natural = Pythagorean(0)
+            static let sharp = Pythagorean(1)
+            static let doubleSharp = Pythagorean(2)
+
+            // MARK: - Computed Properties
+
+            /// Adjustment to letter name in semitones.
+            var adjustment: Double {
+                return Double(amount)
+            }
+
+            // MARK: - Instance Properties
+
+            /// The amount of semitones above or below a given letter name.
+            let amount: Int
+
+            // MARK: - Initializers
+
+            /// Creates a `Pythagorean` base modifier with the given `amount`.
+            init(_ amount: Int) {
+                self.amount = amount
+            }
+        }
+
+        /// Alteration to the base pythagorean modifier.
+        enum Alteration {
+
+            /// Alteration by one syntonic comma (81/80). +/- 21.5 cents.
+            case ptolemaic(Int)
+
+            /// Alteration by one septimal comma (64/63). +/- 27.3 cents.
+            case septimal(Int)
+
+            /// Alteration by one undecimal quarter-tone (33/32). +/- 53.3 cents.
+            case undecimal(Int)
+
+            /// Alteration by one tridecimal third tone (27/26). +/- 65.3 cents.
+            case tridecimal(Int)
+
+            /// - Returns: The amount of adjustment in semitones.
+            var adjustment: Double {
+                switch self {
+                case .ptolemaic(let amount): return 0.215 * Double(amount)
+                case .septimal(let amount): return 0.273 * Double(amount)
+                case .undecimal(let amount): return 0.535 * Double(amount)
+                case .tridecimal(let amount): return 0.653 * Double(amount)
+                }
+            }
+        }
+
+        // MARK: - Computed Properties
+
+        /// The adjustment to letter name in semitones.
+        var adjustment: Double {
+            return base.adjustment + alterations.map { $0.adjustment }.sum
+        }
+
+        // MARK: Instance Properties
+
+        /// The natural, sharp, or flat base modifier.
+        let base: Pythagorean
+
+        /// The additional alterations made to the base modifier.
+        let alterations: [Alteration]
+
+        // MARK: - Initializers
+
+        /// Creates a `Modifier` with the given `Pythagorean` base modifier and the given
+        /// `Alteration` values.
+        init(_ base: Pythagorean = .natural, _ alterations: [Alteration] = []) {
+            self.base = base
+            self.alterations = alterations
+        }
+    }
+}
+
 extension Pitch {
 
     /// Spelled representation of a `Pitch`.
@@ -271,3 +361,4 @@ extension Pitch.Spelling where Tuning == EDO12 {
         return LineOfFifths.distance(ofPitchSpelling: self)
     }
 }
+
