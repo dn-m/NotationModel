fe9087197254b0a7c81e5e6e5da98ac0127cce88
Ben Wetherfield <bwetherfield@gmail.com>
2018-11-07 11:15:37 -0500

    Switch to computed property version

--- Sources/SpelledPitch/PitchSpeller/Wetherfield/UndirectedGraphSchemeProtocol.swift
+++ Sources/SpelledPitch/PitchSpeller/Wetherfield/UndirectedGraphSchemeProtocol.swift
@@ -8,24 +8,3 @@
 import DataStructures
 
 public protocol UndirectedGraphSchemeProtocol: GraphSchemeProtocol where Edge == UnorderedPair<Node> { }
-
-extension UndirectedGraphSchemeProtocol where Self: WeightedGraphSchemeProtocol {
-    
-    func directed <D> () -> D where
-        D: DirectedGraphSchemeProtocol & WeightedGraphSchemeProtocol,
-        D.Weight == Weight,
-        D.Node == Node
-    {
-        return .init { edge in self.weight(from: edge.a, to: edge.b) }
-    }
-}
-
-extension UndirectedGraphSchemeProtocol where Self: UnweightedGraphSchemeProtocol {
-    
-    func directed <D> () -> D where
-        D: DirectedGraphSchemeProtocol & UnweightedGraphSchemeProtocol,
-        D.Node == Node
-    {
-        return .init { edge in self.contains(from: edge.a, to: edge.b) }
-    }
-}
