1b6ba1abec1d2f1d3218689d3c7594a879985c42
Ben Wetherfield <bwetherfield@gmail.com>
2018-11-06 23:09:21 -0500

    Remove .inits

--- Sources/SpelledPitch/PitchSpeller/Wetherfield/UnweightedGraphSchemeProtocol.swift
+++ Sources/SpelledPitch/PitchSpeller/Wetherfield/UnweightedGraphSchemeProtocol.swift
@@ -18,18 +18,18 @@ public protocol UnweightedGraphSchemeProtocol: GraphSchemeProtocol {
 extension UnweightedGraphSchemeProtocol {
     @inlinable
     func pullback <H> (_ f: @escaping (H.Node) -> Node) -> H where H: UnweightedGraphSchemeProtocol {
-        return H.init { self.contains(Edge(f($0.a),f($0.b))) }
+        return H { self.contains(Edge(f($0.a),f($0.b))) }
     }
 }
 
 extension UnweightedGraphSchemeProtocol where Self: UndirectedGraphSchemeProtocol {
     
     static func * (lhs: Self, rhs: Self) -> Self {
-        return Self.init { edge in lhs.contains(edge) && rhs.contains(edge) }
+        return Self { edge in lhs.contains(edge) && rhs.contains(edge) }
     }
     
     static func + (lhs: Self, rhs: Self) -> Self {
-        return Self.init { edge in lhs.contains(edge) || rhs.contains(edge) }
+        return Self { edge in lhs.contains(edge) || rhs.contains(edge) }
     }
 }
 
@@ -39,7 +39,7 @@ extension UnweightedGraphSchemeProtocol where Self: DirectedGraphSchemeProtocol 
         Scheme: UnweightedGraphSchemeProtocol,
         Scheme.Node == Node
     {
-        return Self.init { edge in lhs.contains(edge) && rhs.contains(from: edge.a, to: edge.b) }
+        return Self { edge in lhs.contains(edge) && rhs.contains(from: edge.a, to: edge.b) }
     }
 }
 
