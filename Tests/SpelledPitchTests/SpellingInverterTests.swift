4689d00c1c8ba1d07fb0d7679b437aa89b86093b
Ben Wetherfield <bwetherfield@gmail.com>
2019-02-16 20:58:35 -0800

    Add test for pitch class 1

--- Tests/SpelledPitchTests/SpellingInverterTests.swift
+++ Tests/SpelledPitchTests/SpellingInverterTests.swift
@@ -24,6 +24,18 @@ class SpellingInverterTests: XCTestCase {
         XCTAssertTrue(spellingInverterEbb.flowNetwork.contains(.internal(.init(.init(1,.down),.down))))
     }
     
+    func testSpellingInverterPitchClass1() {
+        let spellingInverterD = SpellingInverter(spellings: [1: Pitch.Spelling(.c, .sharp)])
+        let spellingInverterCx = SpellingInverter(spellings: [1: Pitch.Spelling(.b,.doubleSharp)])
+        let spellingInverterEbb = SpellingInverter(spellings: [1: Pitch.Spelling(.d,.flat)])
+        XCTAssertTrue(spellingInverterD.flowNetwork.contains(.internal(.init(.init(1,.up),.up))))
+        XCTAssertTrue(spellingInverterD.flowNetwork.contains(.internal(.init(.init(1,.down),.down))))
+        XCTAssertTrue(spellingInverterCx.flowNetwork.contains(.internal(.init(.init(1,.up),.up))))
+        XCTAssertTrue(spellingInverterCx.flowNetwork.contains(.internal(.init(.init(1,.down),.up))))
+        XCTAssertTrue(spellingInverterEbb.flowNetwork.contains(.internal(.init(.init(1,.up),.down))))
+        XCTAssertTrue(spellingInverterEbb.flowNetwork.contains(.internal(.init(.init(1,.down),.down))))
+    }
+    
     func testSpellingInverterPitchClass2() {
         let spellingInverterD = SpellingInverter(spellings: [1: Pitch.Spelling(.d)])
         let spellingInverterCx = SpellingInverter(spellings: [1: Pitch.Spelling(.c,.doubleSharp)])
